22b9a2a1392dc56a9a4649ee6252c6971abf2483
Anastasia <anastbulokhova@gmal.com>
2023-10-24 18:05:21 +0300

    Writing code p.2

--- Test_task/Program.cs
+++ Test_task/Program.cs
@@ -3,21 +3,27 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 // лучше обойтись исключительно массивами.
 
-
-int n = 1;
-string[] strs = new string[n];
-for (int i = 0; i < n; i++)
+string[] GetArray(int length = 1)
 {
-    Console.Write("Введите строку {0}:\r\n    ", i + 1);
-    strs[i] = Console.ReadLine();
-}
-Console.WriteLine("Вы ввели следующие значения:");
-for (int i = 0; i < n; i++)
-{
-    Console.WriteLine(strs[i]);
+    // int n = 1;
+    string[] strs = new string[length];
+    for (int i = 0; i < strs.Length; i++)
+    {
+        Console.Write("Введите строку {0}:\r\n    ", i + 1);
+        strs[i] = Console.ReadLine();
+    }
+    Console.WriteLine("Вы ввели следующие значения:");
+    for (int i = 0; i < strs.Length; i++)
+    {
+        Console.WriteLine(strs[i]);
+    }
+    return strs;
 }
 
 string GetNewArray(string[] array)
 {
 
 }
+
+
+string[] array = GetArray();

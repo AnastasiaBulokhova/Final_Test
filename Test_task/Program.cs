54c44cac73b9050cb2d553e41f6aff62dfb1bb9b
Anastasia <anastbulokhova@gmal.com>
2023-10-25 14:01:28 +0300

    Writing code p.5

--- Test_task/Program.cs
+++ Test_task/Program.cs
@@ -3,13 +3,26 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 // лучше обойтись исключительно массивами.
 
-int length = 1;
-
-string[] strs = new string[length];
-for (int i = 0; i < strs.Length; i++)
+string[] GetArray(int length = 1)
 {
-    Console.Write("Введите строку {0}:\r\n    ", i + 1);
-    strs[i] = Console.ReadLine();
+    string[] strs = new string[length];
+    for (int i = 0; i < strs.Length; i++)
+    {
+        Console.Write("Введите строку {0}:\r\n    ", i + 1);
+        strs[i] = Console.ReadLine();
+    }
+    return strs;
+}
+
+int ArraySize(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            count++;
+    }
+    return count;
 }
 
 string[] NewArray(string[] array, int size)
@@ -20,7 +33,7 @@ string[] NewArray(string[] array, int size)
     {
         if (array[i].Length <= 3)
         {
-            newArray[newIndex] = array[i];
+            newArray[idx] = array[i];
             idx++;
         }
     }
@@ -32,14 +45,18 @@ void PrintArray(string[] array)
 {
     int count = array.Length;
 
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        System.Console.Write($"{array[i]} ");
+        Console.Write(array[i]);
+        if(i < array.Length - 1)
+            Console.Write(", ");
     }
     System.Console.WriteLine();
 }
 
 
-PrintArray(strs);
-Console.WriteLine("->");
+string[] array = GetArray();
+Console.WriteLine("-->");
+int size = ArraySize(array);
+string[] newArray = NewArray(array, size);
 PrintArray(newArray);

bc3755ad490efebf179a624092435099bb27daee
Anastasia <anastbulokhova@gmal.com>
2023-10-25 15:33:16 +0300

    Writing code p.6

--- Test_task/Program.cs
+++ Test_task/Program.cs
@@ -3,15 +3,17 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 // лучше обойтись исключительно массивами.
 
+
+
 string[] GetArray(int length = 1)
 {
-    string[] strs = new string[length];
-    for (int i = 0; i < strs.Length; i++)
+    string[] array = new string[length];
+    for (int i = 0; i < length; i++)
     {
         Console.Write("Введите строку {0}:\r\n    ", i + 1);
-        strs[i] = Console.ReadLine();
+        array[i] = Console.ReadLine();
     }
-    return strs;
+    return array;
 }
 
 int ArraySize(string[] array)
@@ -40,11 +42,8 @@ string[] NewArray(string[] array, int size)
     return newArray;
 }
 
-
 void PrintArray(string[] array)
 {
-    int count = array.Length;
-
     for (int i = 0; i < array.Length; i++)
     {
         Console.Write(array[i]);
@@ -58,5 +57,5 @@ void PrintArray(string[] array)
 string[] array = GetArray();
 Console.WriteLine("-->");
 int size = ArraySize(array);
-string[] newArray = NewArray(array, size);
-PrintArray(newArray);
+string[] fixedArray = NewArray(array, size);
+PrintArray(fixedArray);
